bb09ae186fdb3b05b88d30d184a5d518cc92ad8d
wu840407 <wu840407@gmail.com>
2025-02-18 19:06:23 +0800

    第二天

--- knowledgebase/urls.py
+++ knowledgebase/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 列表頁面：顯示所有文章
     path('', views.article_list, name='article_list'),
+    # 詳細頁面：根據文章 ID 顯示單篇文章內容
     path('<int:article_id>/', views.article_detail, name='article_detail'),
 ]
